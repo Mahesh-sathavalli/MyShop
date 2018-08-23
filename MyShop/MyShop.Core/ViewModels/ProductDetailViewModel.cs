2e1ccb6248410bc210b51a503109b9680bf8cb8b
swg\kbadamikar <kaustubh.badamikar@aptean.com>
2018-08-23 20:22:04 +0530

    product and category view
    product and category view

---
+++ MyShop/MyShop.Core/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    public class ProductDetailViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
